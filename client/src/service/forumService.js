5c8ca4cc0974e575f5334783c7d866e88c548e78
TodorMalchov <toshko_01@abv.bg>
2023-12-11 23:52:32 +0200

    create new request service

--- client/src/service/forumService.js
+++ client/src/service/forumService.js
@@ -2,19 +2,17 @@ import constants from './constants.js'
 import { request } from './requests.js'
 
 export const getAll = async()=>{
-    const response = await request(constants.GET,constants.forumURL)
+    const response = await request('GET',constants.forumURL)
     return Object.values(response)
+}
+
+export const getOneComment = async(forumId)=>{
+    const respose = await request('GET', `http://localhost:3030/jsonstore/forum/01b4aa7d-2046-4cb6-9b7e-cf3d11f71135`)
+    return respose
 }
 
 
 export const create = async(forumData)=>{
-    const response = await fetch(constants.forumURL,{
-        method: constants.POST,
-        headers:{
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(forumData)
-    })
-    const result = await response.json()
-    return result
+    const response = await request('POST',constants.forumURL, forumData)
+    return response
 }
--- client/src/service/requests.js
+++ client/src/service/requests.js
@@ -1,5 +1,19 @@
+const buildOptions = (data)=>{
+    const options = {}
+
+    if (data){
+        options.body = JSON.stringify(data)
+        options.header = {
+            'content-type': 'application/json'
+        }
+    }
+    return options
+}
+
+
 export async function request (method,url,data){
     const respose = await fetch(url,{
+        ...buildOptions(data),
         method,
     })
 
