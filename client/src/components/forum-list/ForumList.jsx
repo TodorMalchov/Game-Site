1d69653d625dce0c4fe96823c6c536540aba4f0c
TodorMalchov <toshko_01@abv.bg>
2023-12-12 21:12:45 +0200

    create ForumList to be dinamyc

--- client/src/components/forum-list/ForumList.jsx
+++ client/src/components/forum-list/ForumList.jsx
@@ -1,51 +1,29 @@
 import { useEffect, useState } from 'react';
-import {getAll} from '../../service/forumService';
+import { getAll } from '../../service/forumService';
+import ForumItems from './forum-items/ForumItems';
 
-import ForumItems from './forum-items/ForumItems'
 
 export default function ForumList() {
 
-    const [forum,setForum] = useState([])
+    const [forum, setForum] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         getAll()
-            .then(result => setForum(result))
-    },[])
+            .then(result => setForum(result));
+    }, []);
 
-    console.log(forum)
+    console.log(forum);
     return (
         <section id="catalog-page">
 
             {forum.map(form => (
-                <ForumItems {...form}/>
+                <ForumItems key={form._id}{...form} />
             ))}
-            {/* <div className="allGames">
-                <div className="allGames-info">
-                    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" />
-                    <h6>Username</h6>
-                    <h2>Action, Fantasy, Comedy, Some more genre, And more</h2>
-                    <a className="details-button">Read More</a>
-                </div>
-            </div>
-            <div className="allGames">
+           
+           {forum.length == 0 && (
+                <h1 className='no-articles'> Be a first who post in this forum</h1>
+           )}
 
-                <div className="allGames-info">
-                    <img src="https://images.pexels.com/photos/1433052/pexels-photo-1433052.jpeg?auto=compress&cs=tinysrgb&w=1600" />
-                    <h6>Username</h6>
-                    <h2>Action, Fantasy, Comedy, Some more genre, And more</h2>
-                    <a className="details-button">Read More</a>
-                </div>
-            </div>
-            <div className="allGames">
-
-                <div className="allGames-info">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Qgte_cXXTtbrpPXfF8kKW7nQWYD2Bejy7A&usqp=CAU" />
-                    <h6>Username</h6>
-                    <h2>Action, Fantasy, Comedy, Some more genre, And more, i nekakvi drugi igri</h2>
-                    <a className="details-button">Read More</a>
-                </div>
-            </div> */}
-                
         </section>
     );
 }
