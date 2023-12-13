f83fc418314ab6cf4d4984729ede9c5144de896b
TodorMalchov <toshko_01@abv.bg>
2023-12-13 21:47:55 +0200

    add comments url in constans

--- client/src/service/constants.js
+++ client/src/service/constants.js
@@ -1,6 +1,4 @@
 export default {
     forumURL: "http://localhost:3030/jsonstore/forum/",
-    commentURL:"http://localhost:3030/coments",
-    POST: "POST",
-    GET: "GET"
+    commentURL:"http://localhost:3030/jsonstore/comments/",
 }
