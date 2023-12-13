03c8cc33f78b7b0204d623613200bde2de563c99
TodorMalchov <toshko_01@abv.bg>
2023-12-13 23:59:11 +0200

    add login and register url in constants.js

--- client/src/service/constants.js
+++ client/src/service/constants.js
@@ -1,4 +1,6 @@
 export default {
     forumURL: "http://localhost:3030/jsonstore/forum/",
-    commentURL:"http://localhost:3030/jsonstore/comments/",
+    commentURL: "http://localhost:3030/jsonstore/comments/",
+    loginUrl: "http://localhost:3030/users/login",
+    registerUrl: "http://localhost:3030/users/register"
 }
