73fbdf55cc16e966dec7f4b2d72e08ff84e0d4ca
TodorMalchov <toshko_01@abv.bg>
2023-12-14 17:47:50 +0200

    create formKeys for all forms

---
+++ client/src/service/formKeys.js
@@ -0,0 +1,11 @@
+const formKeys = {
+    Email: 'email',
+    Password: 'password',
+    ConfirmPassword: 'confirm-password',
+    Username: 'username',
+    ImageUrl: 'imageUrl',
+    FavGames: 'favGames',
+    Description: 'description'
+}
+
+export default formKeys
