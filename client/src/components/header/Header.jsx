d34d41e17dc49784b4c0d20ad01cf61697b729f9
TodorMalchov <toshko_01@abv.bg>
2023-12-14 18:30:26 +0200

    create dynamic nav bar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,19 +1,30 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import AuthContext from "../../contexts/authContext"
+
 export default function Header(){
+    const {
+        isAuthenticated 
+    } = useContext(AuthContext)
+    
     return (
         <header>
             <h1><Link className="home" to="/">GamePulse</Link></h1>
             <nav>
                 <Link to="/forum">Forum</Link>
-                <div id="user">
+                {isAuthenticated &&(
+                    <div id="user">
                     <Link to="/create">Add to forum</Link>
                     <Link to="#">Logout</Link>
                     <Link to="#">Special Game</Link>
                 </div>
-                <div id="guest">
+                )}
+                {!isAuthenticated &&(
+                    <div id="guest">
                     <Link to="/login">Login</Link>
                     <Link to="/register">Register</Link>
                 </div>
+                )}
             </nav>
         </header>
     )}
